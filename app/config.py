f18baec6022b4320b387fc7fedd3644ab0f34c85
Jevon Forrest <jevonforrest77@gmail.com>
2022-03-19 12:21:50 -0500

    updating heroku

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     UPLOAD_FOLDER = 'C:\\Users\\Arcan\\Documents\\WebDevLab\\info3180-project1\\uploads'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://admin:Password123@localhost/project1').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://beeqxzsuxbqloc:b6563457787b0a6fddbccb90bf71772f65411ff8b5fa7bdb2806e934ba0ed5db@ec2-3-216-221-31.compute-1.amazonaws.com:5432/d26uug5dfjq6k1'
+).replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
